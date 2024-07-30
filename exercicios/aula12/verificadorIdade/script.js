42d80b1d0046b9aab23ce8ecbc08ddd47d6bdb10
enzoribas <enzortorres2005@gmail.com>
2024-07-29 22:19:55 -0300

    studying

--- exercicios/aula12/verificadorIdade/script.js
+++ exercicios/aula12/verificadorIdade/script.js
@@ -8,6 +8,34 @@ function verificar() {
     } else {
         var fSex = document.getElementsByName('radsex')
         var idade = ano - Number(fAno.value)
-        res.innerHTML = `Idade calculada: ${idade}\nSexo: ${fSex}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fSex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Kid
+                img.setAttribute('src', ) 
+            } else if (idade < 21) {
+                // Teen
+            } else if (idade < 50) {
+                // Adult
+            } else {
+                // Granny
+            }
+        } else { 
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Kid
+            } else if (idade < 21) {
+                // Teen
+            } else if (idade < 50) {
+                // Adult
+            } else {
+                // Granny
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
     }
 }
