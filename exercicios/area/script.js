e7e97fd769169b90e9a26344641361e1a6d7dd6a
Enzo Ribas <135893033+enzortorres@users.noreply.github.com>
2024-08-11 22:32:11 -0300

    studying

--- exercicios/area/script.js
+++ exercicios/area/script.js
@@ -3,5 +3,5 @@ const PI = Math.PI
 
 let raio = 10
 let area = PI * raio ** 2
-area = parseFloat(area.toFixed(2))
+area = parseFloat(area.toFixed(2)) // para definer a quantidade de números após o "."
 console.log(`O resultado da area é ${area}m²`)
