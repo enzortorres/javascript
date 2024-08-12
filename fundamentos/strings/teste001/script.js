f300cfa5b48ff52a30ad0f5a70abaec394fbcc9c
Enzo Ribas <enzortorres2005@gmail.com>
2024-08-12 20:23:52 -0300

    studying

--- fundamentos/strings/teste001/script.js
+++ fundamentos/strings/teste001/script.js
@@ -14,7 +14,10 @@ console.log(universidade.substring(1)) // mostre a string a partir do indíce 1
 console.log(universidade.substring(0,3)) // mostre a string a partir do indíce 0 até o indíce 3
 
 console.log('universidade '.concat(universidade).concat('!'))
+console.log('universidade ' + universidade + '!')
 console.log(universidade.replace('e', 3))
 console.log(universidade.replace(/\w/g, 3)) // /\d/ é uma expressão regular, serve para substituir todos os digítos. /\w\ para as letras e se botar a tag g no final substituí tudo.
 
 console.log('Ana, Maria, Pedro'.split(','))
+const up = text => text.toUpperCase()
+console.log(`Ei... ${up('cuidado')}!`)
---
+++ fundamentos/strings/teste002/script.js
@@ -0,0 +1,7 @@
+const nome = 'Enzo'
+const concatenacao = 'Olá ' + nome + '!'
+const template = `
+    Olá
+    ${nome}!`
+    
+console.log(concatenacao, template)
