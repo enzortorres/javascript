cb62e880f3a2e5d209c6bf8b3fef2f30082445aa
Enzo Ribas <135893033+enzortorres@users.noreply.github.com>
2024-08-14 16:03:07 +0000

    projeto calculadora

--- miniprojetos/calculadora/script.js
+++ miniprojetos/calculadora/script.js
@@ -1,25 +1,62 @@
-var numero1 = []
-var numero2 = []
+var string1 = ''
+var string2 = ''
 var oper = ''
 var teste = 0
+var soma = 0
+var calculo
 
+function init() {
+    calculo = document.querySelector('header#calculo')
+}
 
 function addNumbers(n) {
-    var calculo = window.document.querySelector('header#calculo');
+    var calculo = document.querySelector('header#calculo')
     calculo.innerHTML += n
     if (teste == 0) {
-        numero1.push(n)
+        string1 += n
     } else {
-        numero2.push(n)
+        string2 += n
     }
 }
 
-function oper(value) {
-    calculo.innerHTML = ' '
-    oper = value
+function operacao(string) {
+    var calculo = document.querySelector('header#calculo')
+    if (string == '**') {
+        var resultado = Number(string1) ** 2
+        calculo.innerHTML = resultado
+    } else if (string == '1/2') {
+        var resultado = Number(string1) / 2
+        calculo.innerHTML = resultado
+    } else if (string == 'C') {
+        if (teste == 0) {
+            string1 -= string1.slice(0, -1)
+            calculo.innerHTML = string1
+        } else {
+            string2 -= string2.slice(0, -1)
+            calculo.innerHTML = string2
+        }
+    } 
+    else {
+        calculo.innerHTML = ''
+        oper = string
+    } 
     teste++
 }
     
 function result() {
+    var calculo = document.querySelector('header#calculo')
 
-}
+    if (oper == '+') {
+        var resultado = Number(string1) + Number(string2)
+        calculo.innerHTML = resultado
+    } else if (oper == '-') {
+        var resultado = Number(string1) - Number(string2)
+        calculo.innerHTML = resultado
+    } else if (oper == '*') {
+        var resultado = Number(string1) * Number(string2)
+        calculo.innerHTML = resultado
+    } else if (oper == '/') {
+        var resultado = Number(string1) / Number(string2)
+        calculo.innerHTML = resultado
+    } 
+} 
