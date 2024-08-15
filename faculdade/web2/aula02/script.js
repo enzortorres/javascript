4ef7c96c9236c883164ac059a2c42cc84f14a1a4
enzortorres <enzortorres2005@gmail.com>
2024-08-15 08:56:33 -0300

    studying

---
+++ faculdade/web2/aula02/script.js
@@ -0,0 +1,11 @@
+var a = Number(prompt('Digite um número'))
+a += 5
+alert('A soma dos valores = ' + a)
+document.write('A soma dos valores = ' + a)
+console.log('A soma dos valores é ' + a)++
+
+var b = Number(prompt('Digite um número'))
+b -= a
+alert(`A subtração dos valores = ${b}`)
+document.write(`A subtração de B e A é ${b}`)
+console.log(`A subtração dos valores = ${b}`)
