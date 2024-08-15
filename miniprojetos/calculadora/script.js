7402daa6f4bd072757ea3f95207c107e888abb1b
Enzo Ribas <enzortorres2005@gmail.com>
2024-08-14 22:19:06 -0300

    studying

--- miniprojetos/calculadora/script.js
+++ miniprojetos/calculadora/script.js
@@ -28,35 +28,63 @@ function operacao(string) {
         var resultado = Number(string1) / 2
         calculo.innerHTML = resultado
     } else if (string == 'C') {
-        if (teste == 0) {
-            string1 -= string1.slice(0, -1)
-            calculo.innerHTML = string1
-        } else {
-            string2 -= string2.slice(0, -1)
-            calculo.innerHTML = string2
-        }
-    } 
-    else {
+        string1 = ''
+        string2 = '' 
+        oper = ''
         calculo.innerHTML = ''
+    } else {
         oper = string
-    } 
-    teste++
+        calculo.innerHTML += string
+        teste++
+    }
 }
-    
+
+function back() {
+    var calculo = document.querySelector('header#calculo').innerHTML
+    document.getElementById('calculo').innerHTML = calculo.substring(0, calculo.length -1)
+    if (teste == 0) {
+        string1 = string1.slice(0, -1)
+    } else {
+        string2 = string2.slice(0, -1)
+    }
+}
+
+function isFloat(value) {
+    return Number.isFinite(value) && !Number.isInteger(value);
+}
+
 function result() {
     var calculo = document.querySelector('header#calculo')
-
     if (oper == '+') {
         var resultado = Number(string1) + Number(string2)
-        calculo.innerHTML = resultado
+        if (isFloat(resultado)) {
+            calculo.innerHTML = resultado.toFixed(3)
+        } else {
+            calculo.innerHTML = resultado
+        }
     } else if (oper == '-') {
         var resultado = Number(string1) - Number(string2)
-        calculo.innerHTML = resultado
-    } else if (oper == '*') {
+        if (isFloat(resultado)) {
+            calculo.innerHTML = resultado.toFixed(3)
+        } else {
+            calculo.innerHTML = resultado
+        }
+    } else if (oper == 'x') {
         var resultado = Number(string1) * Number(string2)
-        calculo.innerHTML = resultado
+        if (isFloat(resultado)) {
+            calculo.innerHTML = resultado.toFixed(3)
+        } else {
+            calculo.innerHTML = resultado
+        }
     } else if (oper == '/') {
         var resultado = Number(string1) / Number(string2)
-        calculo.innerHTML = resultado
+        if (isFloat(resultado)) {
+            calculo.innerHTML = resultado.toFixed(3)
+        } else {
+            calculo.innerHTML = resultado
+        }
     } 
+    if (teste = 1) {
+        teste = 0
+    }
 } 
