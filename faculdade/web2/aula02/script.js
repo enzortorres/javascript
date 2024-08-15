6a86a283f4f6017aac4b39eb780893978025a2fa
enzortorres <enzortorres2005@gmail.com>
2024-08-15 11:53:56 -0300

    tde

--- faculdade/web2/aula02/script.js
+++ faculdade/web2/aula02/script.js
@@ -1,11 +1,15 @@
-var a = Number(prompt('Digite um número'))
-a += 5
-alert('A soma dos valores = ' + a)
-document.write('A soma dos valores = ' + a)
-console.log('A soma dos valores é ' + a)++
+// var a = Number(prompt('Digite um número'))
+// a += 5
+// alert('A soma dos valores = ' + a)
+// document.write('A soma dos valores = ' + a)
+// console.log('A soma dos valores é ' + a)++
 
-var b = Number(prompt('Digite um número'))
-b -= a
-alert(`A subtração dos valores = ${b}`)
-document.write(`A subtração de B e A é ${b}`)
-console.log(`A subtração dos valores = ${b}`)
+// var b = Number(prompt('Digite um número'))
+// b -= a
+// alert(`A subtração dos valores = ${b}`)
+// document.write(`A subtração de B e A é ${b}`)
+// console.log(`A subtração dos valores = ${b}`)
+
+for (var i = 0; i <= 5; i++) { // Gerador de numero aleatorio
+    console.log((Math.random()*10).toFixed(0))
+}
---
+++ faculdade/web2/tde/script.js
@@ -0,0 +1,53 @@
+// EXERCÍCIO 1
+
+// let number = +(prompt("Digite um número: "))
+// let number2 = +(prompt("Digite um número: "))
+// let number3 = +(prompt("Digite um número: "))
+
+// document.write(`${(number *2) * number2 /2 }<br>`)
+// document.write(`${(number * 3) + number3}<br>`)
+// document.write(`${number3 ** 3}<br>`)
+
+// EXERCÍCIO 2
+
+// let number = +(prompt("Digite um número: "))
+// document.write(`O numero digitado foi: ${number}`)
+
+// EXERCÍCIO 3
+
+// let metros = +(prompt("Digite um valor em metros: "))
+// let cm = metros * 100
+// let mm = metros * 1000
+// document.write(`Valor digitado: ${metros}m<br>Em centímetros: ${cm}cm<br> Em milímetros: ${mm}mm`)
+
+// EXERCÍCIO 4
+
+// let f = +(prompt("Temperatura em Fahrenheit: "))
+
+// c = 5 * ((number-32) / 9)
+// document.write(`Temperatura convertida para Celsius: ${c.toFixed(2)} graus`)
+
+// EXERCÍCIO 5
+
+// let number = +(prompt("Digite um numero para ver sua tabuada: "))
+
+// for (var i = 1; i <= 10; i++) {
+//     document.write(`${number} x ${i} = ${number * i}<br>`)
+// }
+
+// EXERCÍCIO 6
+// US$1,00 = R$5.40
+
+// let number = +(prompt("Quanto vc tem na carteira: "))
+// let dollars = number / 5.40
+// document.write(`Com esse saldo voce consegue comprar US$${dollars.toFixed(2)}`)
+
+// EXERCÍCIO 7
+
+// let altura = +(prompt("Digite a altura da parede em metros: "))
+// let largura = +(prompt("Digite a largura da parede em metros: "))
+
+// area = altura * largura
+// tinta = area / 2
+
+// document.write(`Essa parede tem uma área de ${area}m² e precisa de ${tinta}L de tinta para cobrir.`)
