4ef6a67952ddd981a150f1f2cb836c11bc9afc5f
Enzo Ribas <enzortorres2005@gmail.com>
2024-08-19 22:58:41 -0300

    studying

--- fundamentos/operadores/destructuring4.js
+++ fundamentos/operadores/destructuring4.js
@@ -6,8 +6,8 @@ function rand([min = 0, max = 1000]) {
     return Math.floor(valor)
 }
 
-console.log(rand([50,40]))
-console.log(rand([992]))
-console.log(rand([, 10]))
-console.log(rand([]))
-console.log(rand())
+console.log('Número aleatório entre 50 e 40, como o min é maior do que o max, o minimo passa a ser o maximo e maximo passa a ser o minimo', rand([50,40]))
+console.log('Número aleatório entre 992 e 1000', rand([992]))
+console.log('Número aleatório entre 0 e 10', rand([ , 10]))
+console.log('Número aleatório entre 0 e 1000', rand([]))
+console.log('ERRO!', rand())
